2d56a51030926f70fe1c7a6c5b7e2239fbe373f2
lukalomidzee <lukalomidze2000@gmail.com>
2025-06-10 19:30:53 +0400

    Remove imports

--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,11 +1,9 @@
 import { Box } from "@mui/material";
-import Header from "../components/Header/Header";
 import Introduction from "../components/Introduction/Introduction";
 import WhoWeAre from "../components/WhoWeAre/WhoWeAre";
 import OurServicesSection from "../components/OurServicesSection/OurServicesSection";
 import GlobalPresenceSection from "../components/GlobalPresenceSection/GlobalPresenceSection";
 import ContactSection from "../components/ContactSection/ContactSection";
-import Footer from "../components/Footer/Footer";
 
 const Homepage = () => {
   return (
